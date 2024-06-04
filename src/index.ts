3f8e4dd85c43b28669b8b8a9c12a1f943d7db64d
岩浆块Magma <magmablock@qq.com>
2024-06-04 22:18:42 +0800

    export const

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { parseFileName as a } from "./parser/parser";
 export const parseFileName = a;
 import { ParseResult as b } from "./result/result";
-export type ParseResult = b;
+export const ParseResult = b;
 import { ExtensionType as c } from "./map/format";
-export type ExtensionType = c;
+export const ExtensionType = c;
 import { Language as d } from "./map/subtitle";
-export type Language = d;
+export const Language = d;
